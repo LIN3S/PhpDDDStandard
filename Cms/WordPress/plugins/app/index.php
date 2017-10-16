a3ef850bcad5fea8a092785f6870393d3b4bbee6
benatespina <benatespina@gmail.com>
2017-10-16 19:28:29 +0200

    Added our app plugin WP entry point

---
+++ Cms/WordPress/plugins/app/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Php DDD Standard project.
+ *
+ * Copyright (c) 2017-present LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+ * Plugin Name: App
+ * Plugin URI: http://lin3s.com
+ * Author: LIN3S
+ * Author URI: http://lin3s.com
+ */
+
+include WP_CONTENT_DIR . '/vendor/autoload.php';
+
+new \App\App();
